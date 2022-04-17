5e9348a3d7c3c4847c19773b23965f82f0f8962f
Shahar <shaharariel95@gmail.com>
2022-04-17 07:11:54 +0300

    day1 update2

---
+++ Ex4/Welcome.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    Welcome <?php echo $_GET["fullName"]; ?><br>
+    Password: <?php echo $_GET["pass"]; ?><br>
+    Your email address is: <?php echo $_GET["mail"]; ?><br>
+    Your website address is: <?php echo $_GET["website"]; ?><br>
+    Your phone address is: <?php echo $_GET["phone"]; ?><br>
+    Your coffee address is: <?php echo $_GET["coffee"]; ?><br>
+    Your temper address is: <?php echo $_GET["temper"]; ?><br>
+    Your address is: <?php echo $_GET["address"]; ?><br>
+    Your interests address is: <?php echo $_GET["interests"]; ?><br>
+    Your age address is: <?php echo $_GET["age"]; ?><br>
+
+</body>
+
+</html>
