f76bdde679d111dd39d6de024e710a06b8271c2e
Shahar <shaharariel95@gmail.com>
2022-06-10 18:59:16 +0300

    Meta changes for portfolio

--- Portfolio/index.php
+++ Portfolio/index.php
@@ -33,7 +33,7 @@ if(mysqli_connect_errno()){
         -->
         
 		<meta http-equiv="X-UA-Compatible" content="IE=Edge">
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
 		<!-- animate -->
